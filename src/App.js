76989b57ac3be76e9b3e98a3438ab5b28bc9fab7
jose iedo <iedoduarte1@gmail.com>
2022-11-15 17:12:01 -0300

    login e elementos globais

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-function App() {
-  return <div>App React</div>;
-}
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
